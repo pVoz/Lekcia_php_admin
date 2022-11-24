89ea481a69330abbedb1957ef5af817b305d6d13
Peter Voz <vozarp93@gmail.com>
2022-11-24 12:12:20 +0100

    Start_lekcia_01

---
+++ admin/index.php
@@ -0,0 +1,23 @@
+<?php include("includes/header.php"); ?>
+
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+
+    <?php include("includes/top_nav.php") ?>
+    <?php include("includes/side_nav.php") ?>
+
+    <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+
+    <!-- /.navbar-collapse -->
+</nav>
+<div id="page-wrapper">
+
+    <?php include("includes/admin_content.php") ?>
+
+
+
+</div>
+<!-- /#page-wrapper -->
+
+<?php include("includes/footer.php"); ?>
